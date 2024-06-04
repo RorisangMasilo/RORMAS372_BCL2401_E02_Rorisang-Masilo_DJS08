17527ca33a383ca35fd27731154b18944e4f8d7d
Rori <rbmasilo@gmail.com>
2024-06-04 11:13:55 +0200

    Added content in about and home pages

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>About</h1>;
+  return (
+    <div className="about-page-container">
+      <img src={bgImg} className="about-hero-image"></img>
+      <div className="about-page-content">
+        <h1>Don't squeeze in a sedan when you relax in a van.</h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </div>
+      <div className="about-page-cta">
+        <h2>
+          Your destination is waiting.<br></br>Your van is ready.
+        </h2>
+        <Link className="link-button" to="/vans">
+          Explore our vans
+        </Link>
+      </div>
+    </div>
+  );
 }
