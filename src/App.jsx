69935316ab5e120e2abfbe769751e4cbb460bf2f
Rori <rbmasilo@gmail.com>
2024-06-04 10:53:13 +0200

    Import React, ReactDOM Implement nested routes Add pages; home and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,38 @@
  * Challenge:
  * Bootstrap the VanLife project by creating the first 2 routes:
  * Home and About.
- * 
+ *
  * Also include the navbar that can link between the two routes.
  * For now, you'll either need to copy/paste the navbar code
  * to both Home and About pages, or you'll need to find a place
  * to put it where it can be shared between the two pages.
  * (Don't overthink this part - just do whatever is easiest for
  * you because we'll learn a better approach very soon)
- * 
+ *
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "pagesHome.jsx";
+import About from "pagesAbout.jsx";
 
 function App() {
-
   return (
-    <h1>Start here</h1>
-  )
+    <BrowserRouter>
+      <header>
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
