d97a363160a77f41fdebe042a8d8556e3b574e33
Rori <rbmasilo@gmail.com>
2024-06-06 09:42:37 +0200

    Active Link styling  with NavLink

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
   return (
     <>
       <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/Reviews">Reviews</Link>
+        <NavLink
+          to="/host"
+          end
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/host/income"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Income
+        </NavLink>
+        <NavLink
+          to="/host/Reviews"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
