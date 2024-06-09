4e151c6ee90e8b4222408901aec12226722eaf54
Rori <rbmasilo@gmail.com>
2024-06-10 00:02:52 +0200

    Add the final Navbar

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, NavLink, Outlet } from "react-router-dom";
 
 export default function HostVanDetail() {
   const { id } = useParams();
@@ -31,6 +31,29 @@ export default function HostVanDetail() {
             <h4>${currentVan.price}/day</h4>
           </div>
         </div>
+        <nav className="host-van-detail-nav">
+          <NavLink
+            to="."
+            end
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Details
+          </NavLink>
+
+          <NavLink
+            to="pricing"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Pricing
+          </NavLink>
+
+          <NavLink
+            to="photos"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Photos
+          </NavLink>
+        </nav>
         <Outlet></Outlet>
       </div>
     </section>
--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,17 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/vans" element={<Vans />}></Route>
-          <Route path="/van/:id" element={<VanDetail />}></Route>
+          <Route path="/van/:id" element={<VanDetail />} />
 
-          <Route path="host" element={<HostLayout />}></Route>
-          <Route index element={<Dashboard />}></Route>
+          <Route path="host" element={<HostLayout />} />
+          <Route index element={<Dashboard />} />
           <Route path="income" element={<Income />}></Route>
           <Route path="reviews" element={<Reviews />}></Route>
           <Route path="vans" element={<HostVans />}></Route>
           <Route path="vans/:id" element={<HostVanDetail />}></Route>
+          <Route index element={<HostVanInfo />} />
+          <Route path="pricing" element={<HostVanPricing />} />
+          <Route path="photos" element={<HostVanPhotos />} R />
         </Route>
       </Routes>
     </BrowserRouter>
