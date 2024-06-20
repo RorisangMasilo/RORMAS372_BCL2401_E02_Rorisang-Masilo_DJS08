4d48d0fa6c61972d61c901e582464988a457caa5
Rori <rbmasilo@gmail.com>
2024-06-21 01:17:47 +0200

    Updated Header component: changed logo text to '#VanLife', replaced className with style for NavLink, added button to log out, and updated login icon image source.

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,33 +9,38 @@ export default function Header() {
     color: "#161616",
   };
 
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <header>
       <Link className="site-logo" to="/">
-        #VANLIFE
+        #VanLife
       </Link>
       <nav>
         <NavLink
           to="/host"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Host
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           About
         </NavLink>
         <NavLink
           to="/vans"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Vans
         </NavLink>
         <Link to="login" className="login-link">
-          <img src="/assets/images/avatar-icon.png" className="login-icon" />
+          <img src={imageUrl} className="login-icon" />
         </Link>
+        <button onClick={fakeLogOut}>X</button>
       </nav>
     </header>
   );
