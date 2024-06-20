3a9e867a9fd7398bdec07702b70986f6ab9eb521
Rori <rbmasilo@gmail.com>
2024-06-21 01:04:26 +0200

    Updated index.html and App.jsx files, removing doctype and meta tags, adding stylesheet link, and reorganizing route configurations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,60 @@
-/* eslint-disable react-refresh/only-export-components */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "../pages/Home.jsx";
-import About from "../pages/About.jsx";
-import Vans from "../pages/Vans/Vans.jsx";
-import VanDetail from "../pages/Vans/VanDetail.jsx";
-import Dashboard from "../pages/Host/Dashboard.jsx";
-import Income from "../pages/Host/Income.jsx";
-import Reviews from "../pages/Host/Reviews.jsx";
-import HostVans from "../pages/Host/HostVans.jsx";
-import HostVanDetail from "../pages/Host/HostVanDetail.jsx";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
-import NotFound from "./pages/NotFound.jsx";
-import Layout from "../components/Layout.jsx";
-import HostLayout from "../components/HostLayout.jsx";
-import AuthRequired from "../components/AuthRequired.jsx";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
+import AuthRequired from "./components/AuthRequired";
 
-import "server.js";
+import "./server";
 
 function App() {
+  /**
+   * Challenge: Create the AuthRequired Layout Route to protect
+   * all the /host routes.
+   *
+   * For now, just use `const authenticated = false`
+   * to determine the authenticated status of the user, and
+   * either send them to the /login route, or render the Outlet
+   */
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/van/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
           <Route path="login" element={<Login />} />
 
-          <Route element={<AuthRequired />} />
-          <Route path="host" element={<HostLayout />} />
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-          <Route path="vans/:id" element={<HostVanDetail />}>
+          <Route element={<AuthRequired />}>
             <Route path="host" element={<HostLayout />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
