fe280eea6d91cd3bf9c1c9a776edfc976c138123
Rori <rbmasilo@gmail.com>
2024-06-21 01:11:23 +0200

    Remove unnecessary parentheses in HostVanPhotos component return statement.

--- pages/Host/HostVanPhotos.jsx
+++ pages/Host/HostVanPhotos.jsx
@@ -3,7 +3,5 @@ import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPhotos() {
   const { currentVan } = useOutletContext();
-  return (
-    <img src={currentVan.imageUrl} className="host-van-detail-image" />;
-    )
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
 }
